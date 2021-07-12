dc498446ea518bd67c2ad1a813088b7ef097f037
Swetha Ramesh <swetharamesh19@gmail.com>
2021-07-12 16:50:33 -0400

    modified install options

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.1.0",
     packages=["counts_to_csv"],
     install_requires=["setuptools_rust"],
-    rust_extensions=[RustExtension("counts_to_csv.counts_to_csv")],
+    rust_extensions=[RustExtension("counts_to_csv.counts_to_csv", debug=False, rust_version="1.53.0")],
     include_package_data=True,
     zip_safe=False,
 )
